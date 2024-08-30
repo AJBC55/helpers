b46b84ec67a425ea4a4ba29b529ac7855b34bd7b
Andrew Chapman <andrew.j.b.chapman@gmail.com>
2024-08-29 20:27:02 -0700

    added the Map function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AJBC55/helpers
+
+go 1.22.0
--- helpers.go
+++ helpers.go
@@ -1,3 +1,10 @@
 package helpers
 
-// helper Map fucntion
+// helper Map function to map an array of one type to another type
+func Map[I, O any](inputArr []I, transformer func(I) O) []O {
+	outputArr := make([]O, len(inputArr))
+	for i := range inputArr {
+		outputArr[i] = transformer(inputArr[i])
+	}
+	return outputArr
+}
